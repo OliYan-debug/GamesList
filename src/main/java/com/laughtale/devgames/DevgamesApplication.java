3423b6833864988d6ab796b0675d03e57ccef065
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2023-07-14 10:28:28 -0300

    First Commit

---
+++ src/main/java/com/laughtale/devgames/DevgamesApplication.java
@@ -0,0 +1,13 @@
+package com.laughtale.devgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laughtale/devgames/DevgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laughtale.devgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
